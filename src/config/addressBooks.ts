5055535032ce3a752f487aeafcd0905edf90f9ef
kassandra <0xdada@protonmail.com>
2022-11-15 20:12:38 -0500

    improve script

--- src/config/addressBooks.ts
+++ src/config/addressBooks.ts
@@ -6,6 +6,9 @@ export interface Addresses {
   ArrakisV2Factory: string;
   UniswapV3Amount: string;
   PALMTerms: string;
+  DevMultisig: string;
+  ProxyMultisig: string;
+  UniswapV3SwapRouter: string;
 }
 /* eslint-enable @typescript-eslint/naming-convention */
 
@@ -20,6 +23,9 @@ export const getAddressBookByNetwork = (network: string) => {
         ArrakisV2Factory: "",
         UniswapV3Amount: "",
         PALMTerms: "",
+        DevMultisig: "",
+        ProxyMultisig: "",
+        UniswapV3SwapRouter: "",
       };
 
     case "mainnet":
@@ -30,6 +36,9 @@ export const getAddressBookByNetwork = (network: string) => {
         ArrakisV2Factory: "0xd9a23005bbe1562856346864BAeb6Ec4d37044Ec",
         UniswapV3Amount: "",
         PALMTerms: "",
+        DevMultisig: "0x5108EF86cF493905BcD35A3736e4B46DeCD7de58",
+        ProxyMultisig: "0xb9229ea965FC84f21b63791efC643b2c7ffB77Be",
+        UniswapV3SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
       };
 
     case "matic":
@@ -39,7 +48,10 @@ export const getAddressBookByNetwork = (network: string) => {
         ArrakisV2Resolver: "0xce3bE976c7c218C15553a4e258CF8734E64A7296",
         ArrakisV2Factory: "0xf5973723eb067F90276d9f9bE80F7C9De67d22af",
         UniswapV3Amount: "0xcCd824d1Baaeb6d6E2B6De867409564F7B8859d2",
-        PALMTerms: "",
+        PALMTerms: "0x48C58178121cBD28833149cb808ce045497A8c4d",
+        DevMultisig: "0xDEb4C33D5C3E7e32F55a9D6336FE06010E40E3AB",
+        ProxyMultisig: "0xd06a7cc1a162fDfB515595A2eC1c47B75743C381",
+        UniswapV3SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
       };
 
     case "goerli":
@@ -50,6 +62,9 @@ export const getAddressBookByNetwork = (network: string) => {
         ArrakisV2Factory: "",
         UniswapV3Amount: "",
         PALMTerms: "",
+        DevMultisig: "",
+        ProxyMultisig: "",
+        UniswapV3SwapRouter: "",
       };
     case "hardhat":
       return {
@@ -59,6 +74,9 @@ export const getAddressBookByNetwork = (network: string) => {
         ArrakisV2Factory: "0xf5973723eb067F90276d9f9bE80F7C9De67d22af",
         UniswapV3Amount: "0xcCd824d1Baaeb6d6E2B6De867409564F7B8859d2",
         PALMTerms: "",
+        DevMultisig: "0x5108EF86cF493905BcD35A3736e4B46DeCD7de58",
+        ProxyMultisig: "0xb9229ea965FC84f21b63791efC643b2c7ffB77Be",
+        UniswapV3SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
       };
 
     default: {
