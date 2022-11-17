6abe63b44d01c7814e33271f9ac4b5ba810fcf88
gevarist <gevariste@protonmail.com>
2022-11-17 15:40:49 +0100

    change require name

--- contracts/abstracts/PALMTermsStorage.sol
+++ contracts/abstracts/PALMTermsStorage.sol
@@ -60,7 +60,9 @@ abstract contract PALMTermsStorage is
         _;
     }
 
-    modifier requireIsOwnerOrDelegate(address vault_) {
+    modifier requireDelegateWhenDelegateExistsOtherwiseRequireIsOwner(
+        address vault_
+    ) {
         address delegate = delegateByVaults[vault_];
         if (delegate != address(0))
             require(msg.sender == delegate, "PALMTerms: no delegate");
@@ -191,7 +193,7 @@ abstract contract PALMTermsStorage is
         external
         override
         requireAddressNotZero(vault_)
-        requireIsOwnerOrDelegate(vault_)
+        requireDelegateWhenDelegateExistsOtherwiseRequireIsOwner(vault_)
     {
         IPALMManager(manager).setVaultData(vault_, data_);
 
@@ -208,7 +210,7 @@ abstract contract PALMTermsStorage is
         external
         override
         requireAddressNotZero(vault_)
-        requireIsOwnerOrDelegate(vault_)
+        requireDelegateWhenDelegateExistsOtherwiseRequireIsOwner(vault_)
     {
         IPALMManager(manager).setVaultStraByName(vault_, strat_);
 
