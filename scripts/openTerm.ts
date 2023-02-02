23a637a22b8166bc85102994531da63e196ca588
kassandra <0xdada@protonmail.com>
2023-02-02 17:49:34 -0500

    chore: udpate scripts

--- scripts/openTerm.ts
+++ scripts/openTerm.ts
@@ -13,32 +13,42 @@ const feeTier = 10000; // uniswap v3 feeTier.
 const token0 = ""; // token0 address.
 const token1 = ""; // token1 address.
 const isAssetTokenZero = true;
-const midAllocationBps = 100;
-const baseAllocationBps = 200;
+const midAllocationBps = 200; // 2% in BPS
+const baseAllocationBps = 100;
 const assetAllocationBps = 300;
-const rangeSize = 2;
-const assetRebalanceThreshold = 1;
+const rangeSize = 2; // 2 * tickSpacing ticks
+const assetRebalanceThreshold = 1.5; // 1.5 * tickSpacing ticks (from outer edge)
 const baseRebalanceThreshold = 1;
-const maxRebalanceGasPrice = 5000000000000; // 500 gwei
-const amount0 = "1";
+const maxRebalanceGasPrice = ethers.utils.parseUnits("50", "gwei").toString(); // 50 gwei
+const amount0 = "1"; // tiny amounts on first deposit
 const amount1 = "1";
 const baseMinRebalanceAmount = "0";
-const assetMinRebalanceAmount = "2";
+const assetMinRebalanceAmount = "2"; // wont rebalance on openTerm since assetMinRebalanceAmount > amount0
 const baseMaxSwapAmount = "1";
 const assetMaxSwapAmount = "1";
 const baseMinVwapAmount = "1";
 const assetMinVwapAmount = "1";
-const twapDuration = 1000;
-const maxTwapDeviation = 100;
-const maxSlippage = 100;
+const twapDuration = 2000; // 33 min (in seconds)
+const maxTwapDeviation = 300; // 300 ticks
 const minTick = -700000;
 const maxTick = 700000;
+const coolDownPeriod = 1000 * 60 * 60; // 60 min (in miliseconds)
 const isBeacon = true;
 const strat = "BOOTSTRAPPING";
-const version = 0.7;
+const version = 1.0;
+const hasApiData = false;
+const apiData = {
+  maxFairPriceDeviation: 300,
+  coingecko: null, // {base: "<id>", asset: "<id>"}
+  coinmarketcap: null, // {base: "<id>", asset: "<id>"}
+};
 
-const gasTankAmount = ethers.utils.parseEther("1");
-const txGasPrice = ethers.utils.parseUnits("20", "gwei");
+const gasTankAmount = 0; // ethers.utils.parseEther("1");
+const hasCustomFeeData = false;
+const customFeeData = {
+  maxFeePerGas: ethers.utils.parseUnits("30", "gwei"),
+  maxPriorityFeePerGas: ethers.utils.parseUnits("1.5", "gwei"),
+};
 
 // #endregion critical input values VERIFY THESE!
 
@@ -67,7 +77,6 @@ async function main() {
     version: version,
     twapDuration: twapDuration,
     maxTwapDeviation: maxTwapDeviation,
-    maxSlippage: maxSlippage,
     baseMaxSwapAmount: baseMaxSwapAmount,
     assetMaxSwapAmount: assetMaxSwapAmount,
     baseMinVwapAmount: baseMinVwapAmount,
@@ -75,7 +84,21 @@ async function main() {
     baseMinRebalanceAmount: baseMinRebalanceAmount,
     assetMinRebalanceAmount: assetMinRebalanceAmount,
     maxGasPrice: maxRebalanceGasPrice,
+    coolDownPeriod: coolDownPeriod,
+    apiData: hasApiData ? apiData : null,
   };
+  const [user] = await ethers.getSigners();
+  const feeData = hasCustomFeeData
+    ? customFeeData
+    : await user?.provider?.getFeeData();
+  if (
+    feeData == undefined ||
+    feeData.maxFeePerGas == undefined ||
+    feeData.maxPriorityFeePerGas == undefined
+  ) {
+    console.log("ERROR: cannot fetch fee data");
+    return;
+  }
   if (
     hre.network.name === "mainnet" ||
     hre.network.name === "polygon" ||
@@ -88,9 +111,15 @@ async function main() {
     console.log("owner:", owner);
     console.log("token0:", token0);
     console.log("token1:", token1);
+    console.log(
+      `\nGas Info:\nMaxFeePerGas: ${Number(
+        ethers.utils.formatUnits(feeData.maxFeePerGas, "gwei")
+      ).toFixed(1)} gwei\nMaxPriorityFeePerGas: ${Number(
+        ethers.utils.formatUnits(feeData.maxPriorityFeePerGas, "gwei")
+      ).toFixed(1)} gwei\n`
+    );
     await sleep(20000);
   }
-  const [user] = await ethers.getSigners();
 
   const dataFormatted = ethers.utils.toUtf8Bytes(JSON.stringify(stratData));
 
@@ -119,15 +148,18 @@ async function main() {
     user
   )) as PALMTerms;
   await t0.approve(addresses.PALMTerms, amount0, {
-    gasPrice: txGasPrice,
+    maxFeePerGas: feeData.maxFeePerGas,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
   });
   const tx1 = await t1.approve(addresses.PALMTerms, amount1, {
-    gasPrice: txGasPrice,
+    maxFeePerGas: feeData.maxFeePerGas,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
   });
   await tx1.wait();
   const tx2 = await terms.openTerm(setupPayload, {
     value: gasTankAmount,
-    gasPrice: txGasPrice,
+    maxFeePerGas: feeData.maxFeePerGas,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
     gasLimit: 2000000,
   });
   console.log("OPENING TERMS:");
